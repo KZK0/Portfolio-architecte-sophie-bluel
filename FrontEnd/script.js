18683d44e5ca7d86046593772e9775fd0b58f4b3
KZKO <med62640@hotmail.fr>
2023-08-23 14:50:43 +0200

    Ajout des projets de façon dynamique, Correction bouton filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,1 +1,30 @@
-const url = ''
+const url = 'http://localhost:5678/api/';
+
+let gallery = [];
+const elemGallery = document.querySelector(".gallery");
+
+
+fetch(url + "works", {
+    method: "GET"
+}).then((result) => {
+    return result.json()
+}).then(data => {
+    gallery = data;
+    displayGallery()
+})
+
+function displayGallery() {
+    for (let i = 0; i < gallery.length; i++) {
+        let fig = document.createElement('figure');
+        let img = document.createElement('img');
+        img.src = gallery[i].imageUrl;
+        fig.appendChild(img)
+        elemGallery.appendChild(fig);
+    }
+}
+
+// function init() {
+
+// }
+
+// init();
