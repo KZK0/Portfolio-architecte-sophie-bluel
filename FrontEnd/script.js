36d0c583fe26d841dad9141a9445bcf660417c4b
KZKO <med62640@hotmail.fr>
2023-08-25 14:20:29 +0200

    Ajout des titres de projet de façon dynamique

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,7 +1,10 @@
+// Constante
+
 const url = 'http://localhost:5678/api/';
 
 let gallery = [];
 const elemGallery = document.querySelector(".gallery");
+const filtres = document.querySelector('.filtres');
 
 
 fetch(url + "works", {
@@ -17,8 +20,11 @@ function displayGallery() {
     for (let i = 0; i < gallery.length; i++) {
         let fig = document.createElement('figure');
         let img = document.createElement('img');
+        let caption = document.createElement('figcaption');
         img.src = gallery[i].imageUrl;
+        caption.textContent = gallery[i].title;
         fig.appendChild(img)
+        fig.appendChild(caption)
         elemGallery.appendChild(fig);
     }
 }
