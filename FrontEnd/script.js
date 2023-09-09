c669b541f2780b7761b0929a5a3b2ea579afe05c
KZKO <med62640@hotmail.fr>
2023-09-09 17:49:10 +0200

    Ajout du tri des projets de la gallery, intégration page login termniner

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,9 +3,18 @@
 const url = 'http://localhost:5678/api/';
 
 let gallery = [];
+let galleryFilter = [];
 let categories = [];
 const elemGallery = document.querySelector(".gallery");
 const filtres = document.getElementById('filtres');
+const login = document.getElementById('login');
+
+login.addEventListener('click', ()=>{
+
+    // window.open('tonurl','_blank')
+    window.location.href='./login.html';
+    login.style.fontWeight = '600';
+})
 
 // ------------- Affichage des projets --------------
 
@@ -14,17 +23,19 @@ fetch(url + "works", {
 }).then((result) => {
     return result.json()
 }).then(data => {
-    gallery = data;
-    displayGallery()
+    gallery  = data;
+    galleryFilter = data;
+    displayGallery(gallery)
 })
 
-function displayGallery() {
-    for (let i = 0; i < gallery.length; i++) {
+function displayGallery(pgallery) {
+    elemGallery.innerHTML = '';
+    for (let i = 0; i < pgallery.length; i++) {
         let fig = document.createElement('figure');
         let img = document.createElement('img');
         let caption = document.createElement('figcaption');
-        img.src = gallery[i].imageUrl;
-        caption.textContent = gallery[i].title;
+        img.src = pgallery[i].imageUrl;
+        caption.textContent = pgallery[i].title;
         fig.appendChild(img)
         fig.appendChild(caption)
         elemGallery.appendChild(fig);
@@ -32,7 +43,6 @@ function displayGallery() {
 }
 
 // ------------- Affichage des catégories --------------
-
 fetch(url + "categories", {
     method: "GET"
 }).then((result) => {
@@ -45,12 +55,27 @@ fetch(url + "categories", {
 function displayCategories() {
     let btn = document.createElement('button')
     btn.textContent = 'Tous'
-    btn.setAttribute('data-id', 0)
+    btn.setAttribute('data-id', 0);    
+    btn.addEventListener('click', (e) => {
+        sortProjects(e)
+    });
+    
     filtres.appendChild(btn)
     for (let i = 0; i < categories.length; i++) {
-        btn = document.createElement('button')
-        btn.textContent = categories[i].name
-        btn.setAttribute('data-id', categories[i].id)
+        btn = document.createElement('button');
+        btn.textContent = categories[i].name;
+        btn.setAttribute('data-id', categories[i].id);
+        btn.addEventListener('click', (e) => {
+            sortProjects(e)
+        });
         filtres.appendChild(btn)
     }
+}
+const sortProjects = (e) => {
+     galleryFilter = gallery.filter(item => item.categoryId == e.target.getAttribute('data-id'));
+     if(galleryFilter.length == 0){       
+        displayGallery(gallery);
+     }else{
+        displayGallery(galleryFilter);
+     }    
 }
